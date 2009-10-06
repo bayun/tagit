e0b37f0e6a935797039eece491a73487277a8ebd
alexx <alexx@telecomax.net>
2009-10-06 19:28:39 +0300

    Added SQL to create tables

---
+++ db.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION;
+CREATE TABLE objects (
+oid INTEGER PRIMARY KEY asc,
+md5 TEXT UNIQUE, 
+source TEXT,
+date INTEGER);
+CREATE TABLE tags (
+tid INTEGER PRIMARY KEY asc,
+name TEXT);
+CREATE TABLE links (
+lid INTEGER PRIMARY KEY asc,
+oid INTEGER NOT NULL,
+tid INTEGER NOT NULL);
+CREATE UNIQUE INDEX md5 on objects (md5);
+CREATE INDEX oid on links (oid);
+CREATE INDEX tid on links (tid);
+COMMIT;
