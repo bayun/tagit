57b9b3d91d6c8f73c158d79a409401bee0c4add8
Bayun <bayun@progressors.org>
2009-10-08 15:38:15 +0300

    Renamed field 'date' to 'addtime' to prevent incompatibility with possible database changes

--- db.sql
+++ db.sql
@@ -3,24 +3,16 @@ CREATE TABLE objects (
 oid INTEGER PRIMARY KEY asc,
 md5 TEXT UNIQUE, 
 source TEXT,
-date INTEGER, title TEXT, description TEXT);
-INSERT INTO "objects" VALUES(1,'cbb2af01694d7deaa351cbe2bc06ffc0','/home/alex/playground/tagit/a.pl',1254873788,'a.pl',NULL);
+addtime INTEGER, 
+title TEXT, 
+description TEXT);
 CREATE TABLE tags (
 tid INTEGER PRIMARY KEY asc,
 name TEXT);
-INSERT INTO "tags" VALUES(1,'a');
-INSERT INTO "tags" VALUES(2,'b');
-INSERT INTO "tags" VALUES(3,'c');
-INSERT INTO "tags" VALUES(4,'a');
-INSERT INTO "tags" VALUES(5,'b');
-INSERT INTO "tags" VALUES(6,'c');
 CREATE TABLE links (
 lid INTEGER PRIMARY KEY asc,
 oid INTEGER NOT NULL,
 tid INTEGER NOT NULL);
-INSERT INTO "links" VALUES(1,1,4);
-INSERT INTO "links" VALUES(2,1,5);
-INSERT INTO "links" VALUES(3,1,6);
 CREATE TABLE attach (aid INTEGER PRIMARY KEY asc,
 pid INTEGER NOT NULL,
 cid INTEGER NOT NULL);
