6f5abf76a176db357197980bc8d7dee447eb5767
Bayun <bayun@progressors.org>
2009-10-20 02:05:44 +0300

    Added preliminary support for name-value attributes
    Added support for setting and deletion of named attributes in 'modify' command. Modified SQL - added new table to store these attributes.
    Currently there are no ways to show these attributes or use them for object filtering

--- db.sql
+++ db.sql
@@ -9,6 +9,13 @@ description TEXT);
 CREATE TABLE tags (
 tid INTEGER PRIMARY KEY asc,
 name TEXT UNIQUE);
+
+CREATE TABLE attrs (
+aid INTEGER PRIMARY KEY asc,
+oid INTEGER NOT NULL,
+name TEXT NOT NULL,
+value TEXT
+);
 CREATE TABLE links (
 lid INTEGER PRIMARY KEY asc,
 oid INTEGER NOT NULL,
@@ -20,4 +27,7 @@ CREATE UNIQUE INDEX hash on objects (hash);
 CREATE INDEX oid on links (oid);
 CREATE INDEX tid on links (tid);
 CREATE INDEX pid on attach (pid);
+CREATE INDEX attr_obj on attrs(oid);
+CREATE INDEX attr_name on attrs(name);
+CREATE UNIQUE INDEX obj_attr on attrs (oid, name);
 COMMIT;
